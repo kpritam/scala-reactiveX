198a9c11323ad628db81296e0dbd1c4e785281a9
Pritam Kadam <kpritam@thoughtworks.com>
2019-02-23 12:06:29 +0530

    format.

--- kvstore/src/main/scala/kvstore/Arbiter.scala
+++ kvstore/src/main/scala/kvstore/Arbiter.scala
@@ -4,23 +4,28 @@ import akka.actor.{ActorRef, Actor}
 import scala.collection.immutable
 
 object Arbiter {
+
   case object Join
 
   case object JoinedPrimary
+
   case object JoinedSecondary
 
   /**
-   * This message contains all replicas currently known to the arbiter, including the primary.
-   */
+    * This message contains all replicas currently known to the arbiter, including the primary.
+    */
   case class Replicas(replicas: Set[ActorRef])
+
 }
 
 class Arbiter extends Actor {
+
   import Arbiter._
+
   var leader: Option[ActorRef] = None
   var replicas = Set.empty[ActorRef]
 
-  def receive = {
+  def receive: Receive = {
     case Join =>
       if (leader.isEmpty) {
         leader = Some(sender)
--- kvstore/src/main/scala/kvstore/Persistence.scala
+++ kvstore/src/main/scala/kvstore/Persistence.scala
@@ -5,18 +5,21 @@ import scala.util.Random
 import java.util.concurrent.atomic.AtomicInteger
 
 object Persistence {
+
   case class Persist(key: String, valueOption: Option[String], id: Long)
+
   case class Persisted(key: String, id: Long)
 
   class PersistenceException extends Exception("Persistence failure")
 
-  def props(flaky: Boolean): Props = Props(classOf[Persistence], flaky)
+  def props(flaky: Boolean): Props = Props(new Persistence(flaky))
 }
 
 class Persistence(flaky: Boolean) extends Actor {
+
   import Persistence._
 
-  def receive = {
+  def receive: Receive = {
     case Persist(key, _, id) =>
       if (!flaky || Random.nextBoolean()) sender ! Persisted(key, id)
       else throw new PersistenceException
