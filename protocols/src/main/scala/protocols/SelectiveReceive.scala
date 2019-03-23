daaaadef03fe533c2058667460c138802b644a15
Pritam Kadam <kpritam@thoughtworks.com>
2019-03-24 00:42:07 +0530

    [Transactor] Selective Receive (All tests passing)

--- protocols/src/main/scala/protocols/SelectiveReceive.scala
+++ protocols/src/main/scala/protocols/SelectiveReceive.scala
@@ -17,7 +17,6 @@ object SelectiveReceive {
     *           `validateAsInitial`, `interpretMessage`,`canonicalize` and `isUnhandled`.
     */
   def apply[T](bufferSize: Int, initialBehavior: Behavior[T]): Behavior[T] = Behaviors.setup { ctx ⇒
-    val buffer = StashBuffer[T](bufferSize)
 
     def interpret(behavior: Behavior[T], msg: T): (Behavior[T], Boolean) = {
       val started = Behavior.validateAsInitial(Behavior.start(behavior, ctx))
@@ -25,24 +24,18 @@ object SelectiveReceive {
       (Behavior.canonicalize(interpreted, started, ctx), Behavior.isUnhandled(interpreted))
     }
 
-    def beh(current: Behavior[T]): ExtensibleBehavior[T] = new ExtensibleBehavior[T] {
-      override def receive(_ctx: ActorContext[T], msg: T): Behavior[T] = {
-        val (canonical, unHandled) = interpret(current, msg)
-        if (unHandled) {
-          println(s"Stashing $msg")
-          buffer.stash(msg)
-          beh(canonical)
+    def beh(current: Behavior[T], buffer: StashBuffer[T]): ExtensibleBehavior[T] =
+      new ExtensibleBehavior[T] {
+        override def receive(_ctx: ActorContext[T], msg: T): Behavior[T] = {
+          val (canonical, unHandled) = interpret(current, msg)
+          if (unHandled) beh(canonical, buffer.stash(msg))
+          else buffer.unstashAll(ctx, beh(canonical, StashBuffer[T](bufferSize)))
         }
-        else buffer.unstashAll(ctx, beh(canonical))
+
+        override def receiveSignal(ctx: ActorContext[T], msg: Signal): Behavior[T] = beh(Behavior.interpretSignal(current, ctx, msg), buffer)
       }
 
-      override def receiveSignal(ctx: ActorContext[T], msg: Signal): Behavior[T] = {
-        println(s"Processing signal $msg")
-        beh(Behavior.interpretSignal(current, ctx, msg))
-      }
-    }
-
-    beh(initialBehavior)
+    beh(initialBehavior, StashBuffer[T](bufferSize))
   }
 
 }
